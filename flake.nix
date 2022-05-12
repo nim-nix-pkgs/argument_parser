dc0c76572e36be47fba9d8f25001e148fed8c633
hugosenari <hugosenari@gmail.com>
2022-05-12 11:49:40 -0300

    chore: re index argument_parser

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."argument_parser-master".dir   = "master";
+  inputs."argument_parser-master".owner = "nim-nix-pkgs";
+  inputs."argument_parser-master".ref   = "master";
+  inputs."argument_parser-master".repo  = "argument_parser";
+  inputs."argument_parser-master".type  = "github";
+  inputs."argument_parser-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."argument_parser-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
